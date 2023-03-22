75a607e4a4a7bef654ad7fae83d639a81f7921e1
PrinceNonso <64286959+PrinceNonso@users.noreply.github.com>
2023-03-22 09:07:51 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment (){
+    setCount ( count +1);
+  }
+  function decrement (){
+    setCount (count -1)
+  }
+  return (
+    <div>
+     <h1> Counter: {count}</h1>
+     <button onClick={increment}>+</button>
+     <button onClick={decrement}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
